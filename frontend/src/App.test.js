d40a20d75136428abf6728f1963d86142eb9f0d6
Michael Pesce <michaelcpesce@gmail.com>
2022-08-04 09:04:53 -0700

    migrated ui contents into electron directory; updated some start scripts

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import * as React from 'react'
-
-test('renders learn react link', () => {
-  render(<BrowserRouter> <App /> </BrowserRouter>);
-});
