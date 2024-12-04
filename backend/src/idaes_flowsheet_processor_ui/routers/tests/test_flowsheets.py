ac9e57e6ac1cb253cabebc5aba5866dbc7f394d9
MichaelPesce <michaelcpesce@gmail.com>
2024-12-04 09:09:59 -0600

    main -> idaes_flowsheet_processor_ui.main

--- backend/src/idaes_flowsheet_processor_ui/routers/tests/test_flowsheets.py
+++ backend/src/idaes_flowsheet_processor_ui/routers/tests/test_flowsheets.py
@@ -13,7 +13,7 @@ os.environ["watertap_packages"] = '["watertap", "examples"]'
 
 import pytest
 from fastapi.testclient import TestClient
-from main import app
+from idaes_flowsheet_processor_ui.main import app
 from idaes_flowsheet_processor_ui.internal import flowsheet_manager as fm
 
 
