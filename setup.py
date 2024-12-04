ebd06ea69c5c3d94875a5ccd046f6a620d4b901b
MichaelPesce <michaelcpesce@gmail.com>
2024-12-04 05:53:00 -0600

    add setup.py, init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="idaes-flowsheet-processor-ui",
+    author="WaterTAP UI developers",
+    license="LICENSE",
+
+    version="22.9.9",
+
+    package_dir={"": "backend/src"},
+    packages=find_packages(where="backend/src"),
+
+    install_requires=[
+    ],
+
+    entry_points={
+        "watertap.flowsheets": [
+        ],
+    }
+)
