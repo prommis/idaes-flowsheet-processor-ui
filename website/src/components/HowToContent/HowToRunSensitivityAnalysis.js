5cee907be324efbd95845108d38d3c3136167074
MichaelPesce <michaelcpesce@gmail.com>
2025-03-25 12:14:58 -0500

    break how to use into different sections and use mdx headers to ensure TOC is created

---
+++ website/src/components/HowToContent/HowToRunSensitivityAnalysis.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const styles = {
+  container: {
+    margin: '40px auto',
+    padding: 20,
+    backgroundColor: '#f9f9f9',
+    border: '1px solid #ddd',
+    borderRadius: 10,
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    width: '100%'
+  },
+  image: {
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.4)',
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 10
+  }
+};
+
+function HowToRunSensitivityAnalysis() {
+
+
+    return (
+    <div style={{marginTop: '20px'}}>
+        <ol>
+            
+        </ol>
+    </div>
+    
+  );
+}
+
+export default HowToRunSensitivityAnalysis;
