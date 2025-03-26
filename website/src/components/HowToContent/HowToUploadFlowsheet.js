f5734f8f27f426f5721a70d8d7f6592c7280b86c
MichaelPesce <michaelcpesce@gmail.com>
2025-03-26 09:09:03 -0500

    add how to upload flowsheet

--- website/src/components/HowToContent/HowToUploadFlowsheet.js
+++ website/src/components/HowToContent/HowToUploadFlowsheet.js
@@ -20,11 +20,26 @@ const styles = {
 
 function HowToUploadFlowsheet() {
 
+  const howToExportLink = 'how_to_export_flowsheet'
+
 
     return (
     <div style={{marginTop: '20px'}}>
         <ol>
-            
+          <li>
+              Navigate to the flowsheets list page. Click on New Flowsheet +. You should see the following:
+              <img 
+                style={styles.image}
+                src={require('@site/static/img/upload-flowsheet-mac.png').default}
+              />
+          </li>
+          <li>
+              A flowsheet python file and an export python file (<a href={howToExportLink} target='_blank'>see how to export a flowsheet</a>) for that flowsheet are required. In addition, you can upload a diagram of the flowsheet and any necessary data files (yaml, json, csv, etc). After attaching the files, click Upload Flowsheet. If successful, you should see the page refresh and see your flowsheet populate in the list. If unsuccessful, you should see an error popup. 
+              <img 
+                style={styles.image}
+                src={require('@site/static/img/custom-flowsheet-mac.png').default}
+              />
+          </li>
         </ol>
     </div>
     
